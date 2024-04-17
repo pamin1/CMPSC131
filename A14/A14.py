96c53357ebc0ed8251ebfc8be87bc5e983183fa2
pamin1 <46505451+pamin1@users.noreply.github.com>
2024-04-17 15:55:26 -0400

    Completed assignment, Commented through code

--- A14/A14.py
+++ A14/A14.py
@@ -5,17 +5,20 @@ def load_data(fn: str) -> tuple[int, list[list[int]]]:
         file = open(fn, "r")
     except FileNotFoundError:
         print("Input file does not exist")
-        return -1
+        return (-1, [])
+    
     # initialize vars, read first line
     out:    list = []
     row:    int  = 0
     count:  int  = 0
     line = file.readline()
+
     # iterate through csv lines
     while line != "":
         # split line, reset cleaned list
         lst = line.split(",")
         fin: list = []
+
         # iterate through the line
         for i in range(len(lst)):
             # if formattable to an int, 
@@ -23,16 +26,21 @@ def load_data(fn: str) -> tuple[int, list[list[int]]]:
             try:
                 lst[i] = int(lst[i])
                 fin += [lst[i]]
+
             # otherwise, we have a non-int,
             # print the row and col it was found at
             except ValueError:
-                print([row, i])
+                print("Row", row, end="")
+                print(", Col", i, end = "")
+                print(" was invalid")
+
         # append the cleaned list, iterate the row,
         # and sum integers loaded, read the next line
         out.append(fin)
         row += 1
         count += len(fin)
         line = file.readline()
+        
     # close file and return information as tuple
     file.close()
     return (count, out)
@@ -40,31 +48,57 @@ def load_data(fn: str) -> tuple[int, list[list[int]]]:
 def save_data(fn: str, data: list[tuple[float, float, tuple[int,...]]]) -> None:
     # open file and initialize list
     file = open(fn, "w")
-    flat:list = []
-    # iterate through data
-    for i in range(len(data)):
-        # if 
-        try:
-            first, *rest = data[i]          
-            flat.append(first)
-            flat.extend(rest)
-        except TypeError:
-            flat.append(data[i]) 
-    for j in range(len(flat)):
-        file.write(flat[j])
 
+    # iterate through entire data list
+    for i in range(len(data)):
+        row = data[i]
+
+        # iterate through each row element
+        for j in range(len(row)):
+            # if the element is a tuple, we need to iterate through that on its own
+            if isinstance(row[j], tuple):
+                for k in range(len(row[2])):
+                    val = str(row[2][k])
+                    file.write(val)
+                    if k < len(row[2]) - 1:
+                        file.write(",")
+
+            # otherwise we can just write in the value 
+            else:
+                val = str(row[j])
+                file.write(val)
+
+            # separate w/ commas up until the second to last value for formatting
+            if j < len(row) - 1:
+                file.write(",")
+        # start new line after every row finishes
+        file.write("\n")         
     file.close()
 
 # Part 2: Processing:
 def find_mean(lst: list[int]) -> float:
+    # assertion
+    assert len(lst) != 0, "List cannot be empty"   
+
+    # perform average calculation over the list elements  
     sum: float = 0
     for i in range(len(lst)):
         sum += lst[i]
     avg = sum / len(lst)
-    avg = round(avg, 1)
-    return avg
+
+    # multiply avg by ten to get tenths into ones place
+    # rounds down for any value with new tenths place < 0.5
+    # likewise can round up for tenths >= 0.5
+    # divide by 10 to bring value back down to original decimal position
+    temp = int(10 * avg - 0.5) + 1
+    temp /= 10
+    return temp
 
 def find_median(lst: list[int]) -> float:
+    # assertion
+    assert len(lst) != 0, "List cannot be empty" 
+
+    # bubble sort for the length of the list
     length = len(lst)
     for j in range(length):
         for i in range(length - 1):
@@ -72,7 +106,8 @@ def find_median(lst: list[int]) -> float:
                 temp = lst[i + 1]
                 lst[i + 1] = lst[i]
                 lst[i] = temp
-    
+
+    # if the list is even, perform calculations to average the middle two index values
     if len(lst) % 2 == 0:
         left  = (len(lst) // 2) - 1
         right = (len(lst) // 2)
@@ -80,35 +115,54 @@ def find_median(lst: list[int]) -> float:
         temp_right = lst[right]
         temp = (temp_left + temp_right) / 2
         return temp
+    
+    # otherwise return the middle index value
     temp = len(lst) // 2
-    return lst[temp]
+    return float(lst[temp])
+
 def find_mode(lst: list[int]) -> tuple[int, ...]:
+    # assertion
+    assert len(lst) != 0, "List cannot be empty" 
+
+    # keep a dictionary of the found frequencies
     freqs: dict[int, int] = {}
-    max: list = [0]
+    max: list = 0
     tup_out: tuple = ()
-    indiv: list = []
+    unique: list = []
+
+    # if we havent seen the value before (freq = 0)
+    # add it to a lst of unique values
+    # then add it to the dict of frequency,
+    # add to current value (0 if not found)
     for i in range(len(lst)):
         if freqs.get(lst[i], 0) == 0: 
-            indiv.append(lst[i])
-
+            unique.append(lst[i])
         freqs[lst[i]] = 1 + freqs.get(lst[i], 0)
-        if freqs[lst[i]] > max[0]:
-            max = [freqs[lst[i]]]
 
-        
-    for j in range(len(indiv)):
-        if freqs[indiv[j]] == max[0]:
-            tup_out += (indiv[j], )
+        # compare frequency value and max, 
+        # update max as necessary
+        if freqs[lst[i]] > max:
+            max = freqs[lst[i]]
+
+    # iterate through the list of unique values,
+    # if the frequency of the value = the max recorded frequency,
+    # add it to the tuple to be returned
+    for j in range(len(unique)):
+        if freqs[unique[j]] == max:
+            tup_out += (unique[j], )
     return tup_out
 
 #Part 3: Analysis:
 def analysis(fn_in: str, fn_out: str) -> None:
+    # intitialize
     file = open(fn_out, "w")
     tup = load_data(fn_in)
     out: list = []
 
-    print("Loaded", str(tup[0]), "integers")
+    # first print statement
+    print("Loaded", tup[0], "integers")
 
+    # iterate and apply functions through the data list
     lst = tup[1]
     for i in range(len(lst)):
         row = lst[i]
@@ -117,11 +171,13 @@ def analysis(fn_in: str, fn_out: str) -> None:
         mode = find_mode(row)
         line = (mean, median, mode)
         out.append(line)
+
+    # save the data and close
     save_data(fn_out, out)
     file.close()
 
 def main():
-    analysis("A14\dataset_1.csv", "A14\out.csv")
+    analysis("A14\dataset_3.csv", "A14\out.csv")
     
 
 if __name__ == "__main__":
